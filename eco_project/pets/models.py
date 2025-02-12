7e8d5e48384943f38cf225e4d9c9e9df7d1c0d15
Kit Matthewson <kit.matthewson@gmail.com>
2025-02-12 10:37:54 +0000

    Create Cosmetics models

--- eco_project/pets/models.py
+++ eco_project/pets/models.py
@@ -1,18 +1,18 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
 
 class PetType(models.Model):
     """
-    A model to store a type of pet
+    A model to store a type of pet (i.e. Dog, Cat)
     """
-    id: models.AutoField = models.AutoField(primary_key=True)
-    name: models.CharField = models.CharField(max_length=200, unique=True)
-    description: models.TextField = models.TextField()
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
 
-    base_image: models.ImageField = models.ImageField(
+    base_image = models.ImageField(
         upload_to='pet_imgs/',
+        name="Base Image",
         blank=False)
 
     def __str__(self):
@@ -22,6 +22,44 @@ class PetType(models.Model):
         :return: The name of the pet type.
         """
         return f'{self.name}'
+
+
+class CosmeticType(models.Model):
+    """
+    A model store a type of cosmetic (i.e. Hat, Scarf)
+    """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    x = models.FloatField(name="X Position")
+    y = models.FloatField(name="Y Position")
+
+    def __str__(self):
+        """
+        Returns a string representation of the object.
+
+        :return: The name of this cosmetic typw.
+        """
+        return f'{self.name}'
+
+
+class Cosmetic(models.Model):
+    """
+    A model to store a specific cosmetic (i.e. Red Scarf)
+    """
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    type = models.ForeignKey(CosmeticType, on_delete=models.PROTECT)
+    fits = models.ManyToManyField(PetType, blank=False)
+
+
+    def __str__(self):
+        """
+        Returns a string representation of the object.
+
+        :return: The name of this cosmetic typw.
+        """
+        return f'{self.name} ({self.type.name})'
 
 class Pet(models.Model):
     """
@@ -33,9 +71,10 @@ class Pet(models.Model):
 
     points = models.IntegerField(default=0)
     health = models.IntegerField(default=100, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    cosmetics = models.ManyToManyField(Cosmetic, blank=True)
 
-    # TODO
-    # owner: models.ForeignKey(...)
+    # TODO Point to user model once it has been created
+    # owner = models.OneToOneField("User", on_delete=models.CASCADE)
 
     def __str__(self):
         """
