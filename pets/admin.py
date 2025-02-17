2c5542203797b9e8d3cc608303aa1a67b6cb38e0
Zara-Brown <ezb201@exeter.ac.uk>
2025-02-17 15:46:03 +0000

    Revert "Leaderboard "

--- pets/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PetType, Pet
-
-admin.site.register(PetType)
-admin.site.register(Pet)
--- pets/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pets'
-
-    def ready(self):
-        import pets.signals
