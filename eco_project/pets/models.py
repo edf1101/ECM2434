f7549ea5868c58018a514f1c75e524e219c330dc
hugoblanco1 <hugo.blanco840@gmail.com>
2025-03-05 11:31:31 +0000

    Update models.py

--- eco_project/pets/models.py
+++ eco_project/pets/models.py
@@ -6,7 +6,7 @@ This module contains the models for the pets app.
 
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
