b01474b19516f1db7cfc041975cb80b026ff7895
Kit Matthewson <kit.matthewson@gmail.com>
2025-03-13 10:06:47 +0000

    Improve the shop page

--- eco_project/pets/views.py
+++ eco_project/pets/views.py
@@ -3,14 +3,13 @@ This module contains the views for the pets app.
 
 @author: 730003140, 730009864, 730020278, 730022096, 730002704, 730019821, 720039505
 """
-
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.checks import messages
 from django.http import HttpResponse
 
 from django.shortcuts import render, get_object_or_404, redirect
 
-from pets.models import Cosmetic
+from pets.models import Cosmetic, CosmeticType
 
 
 @login_required
@@ -24,19 +23,6 @@ def view_pet(request) -> HttpResponse:
 
     pet = request.user.pets.first()
     return render(request, "pets/mypet.html", { "pet": pet })
-
-
-@login_required
-def shop(request) -> HttpResponse:
-    """
-    View for the pet accessories shop
-
-    @param request: HttpRequest object
-    @return: HttpResponse object
-    """
-    profile = request.user.profile
-    all_items = Cosmetic.objects.all()
-    return render(request, "pets/shop.html", {"profile": profile, "items": all_items})
 
 
 @login_required
