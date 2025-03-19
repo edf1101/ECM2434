675e21796af3b7c8a9ccb3f03f745a5e7a4b7543
Ed <edfillingham1@gmail.com>
2025-03-19 15:24:52 +0000

    basic lootboxes

--- eco_project/lootboxes/urls.py
+++ eco_project/lootboxes/urls.py
@@ -1,12 +1,12 @@
 """
-This module contains the lootboxes app's URL configuration.
+This file is used to define the URL patterns for the lootboxes app.
 """
+
 from django.urls import path
 from . import views
 
-app_name = "lootboxes"
-
+app_name = 'lootboxes'
 urlpatterns = [
-    path("spin/", views.spin_lootbox, name="spin_lootbox"),
-
+    path('', views.wheel_view, name='spin'),
+    path('api/spin/', views.spin_wheel, name='spin_wheel'),
 ]
